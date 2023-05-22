d920653b50394c44223649603d7036a8b1fda55f
Dan97O <dan97-oddo@live.it>
2023-05-22 14:22:28 +0200

    add link bootstrap and fontawesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Home</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
+        <!-- Link Bootstrap css -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- Link Fontawesome -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css">
     </head>
+
     <body>
-            <h1>
-                {{$page_title}}
-            </h1>
+        <header class="d-flex justify-content-center mt-5">
+            <h1>{{$page_title}} <i class="fa-brands fa-laravel"></i></h1>
+        </header>
     </body>
+
 </html>
